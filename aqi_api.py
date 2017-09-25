ca86ab650aecf31c37d65e99c315c7ccd925e6c7
Michael Jolliffe <michael.a.jolliffe@gmail.com>
2017-09-24 18:57:06 -0700

    subsumed inside function, added a build_report that grabs relevant info from json object and displays it in user-friendly way

--- aqi_api.py
+++ aqi_api.py
@@ -3,20 +3,43 @@
 import requests
 from api_keys import AQI_TOKEN, LATLONG
 
-def get_aq_data():
-    '''Sends a get request to aqicn API'''
-    params = {
-                'token': AQI_TOKEN, #TODO get api token, store in gitignore file
-                'latlong': LATLONG
-    }
+class AirQualityReport:
+    def __init__(self):
+        self.category = { 50: 'Good',
+                          100: 'Moderate',
+                          150: 'Unhealthy for Sensitive Groups',
+                          200: 'Unhealthy',
+                          300: 'Very Unhealthy',
+                          500: 'Hazardous'
+                        }
 
-    request_url = f'http://api.waqi.info/feed/geo:{LATLONG}/?token={params["token"]}'
-    resp = requests.get(request_url)
+        self.currentAQI = None
+        self.currentCat = None
 
-    return resp.json()
+    def get_aq_data(self):
+        '''Sends a get request to aqicn API'''
+        params = {
+                    'token': AQI_TOKEN, #TODO get api token, store in gitignore file
+                    'latlong': LATLONG
+        }
+
+        request_url = f'http://api.waqi.info/feed/geo:{LATLONG}/?token={params["token"]}'
+        resp = requests.get(request_url)
+
+        return resp.json()
+
+    def build_aq_report(self, json_response):
+        '''Grabs the relevant info and leaves the rest. Builds a short report for texting to user.'''
+        self.currentAQI = json_response['data']['aqi']
+        for key in self.category:
+            if self.currentAQI < key:
+                self.currentCat = self.category[key]
+                break
+        return f'Current Air Quality: {self.currentAQI}, {self.currentCat}'
 
 if __name__ == '__main__':
-    print(get_aq_data())
+    report = AirQualityReport()
+    print(report.build_aq_report(report.get_aq_data()))
 
     #'feed/:city/?token=:token'
     #http://api.waqi.info/feed/shanghai/?token=demo
